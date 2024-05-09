0b54a64be83df61daae530591c21ce51dfbbb29e
Phan Tuan Kiet <phankietptk04231@gmail.com>
2024-05-09 09:29:12 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once "./config/db.php";
+require_once "./model/master.php";
+if(isset($_GET["controller"],$_GET["action"])){
+    $controller = $_GET["controller"];
+    $action = $_GET["action"];
+}else{
+$controller = "Home";
+$action = "index";
+}
+require_once "./view/layout.php"
+?>
